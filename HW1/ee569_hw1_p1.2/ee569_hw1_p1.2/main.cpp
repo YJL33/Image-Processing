7a7943f57b0bb7862e7ecda60ad467797fb0b83e
YJL33 <yunjun.l33@gmail.com>
2015-09-04 12:50:00 -0700

    Modify the folder structure

---
+++ HW1/ee569_hw1_p1.2/ee569_hw1_p1.2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ee569_hw1_p1.2
+//
+//  Created by Yun-Jun Lee on 9/4/15.
+//  Copyright (c) 2015 USC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
